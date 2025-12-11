9b241f10fe5e6bd03b05576e216b83a3ac738eb3
Yamini-519 <24331a05i9@mvgrce.edu.in>
2025-12-11 19:49:16 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,8 @@
+import math
+print("Value of pi:", math.pi)
+num = 36
+print("Square root of", num, "is:", math.sqrt(num))
+angle = 60  
+radians = math.radians(angle)  
+print("sin(60°) =", math.sin(radians))
+print("cos(60°) =", math.cos(radians))
